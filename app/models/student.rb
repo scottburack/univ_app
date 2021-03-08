1552f81569b7c3070894a85bfed4cdc100eda4a0
Scott <itsmescottb123@gmail.com>
2021-03-08 17:37:57 -0500

    student table and validations

---
+++ app/models/student.rb
@@ -0,0 +1,10 @@
+class Student < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { minimum: 2, maximum: 25 }
+
+  VALID_EMAIL_REGEX = /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/
+  validates :email, presence: true, length: { minimum: 3, maximum: 75 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGEX }
+
+end
