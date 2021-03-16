6deb8e51fae0ee856025c631fe18673a65c057d9
Scott <itsmescottb123@gmail.com>
2021-03-16 18:06:25 -0400

    style signup message and errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,4 +23,5 @@ import 'materialize-css/dist/js/materialize'
 $(document).on('turbolinks:load', function() {
   $(".dropdown-trigger").dropdown();
   $('.sidenav').sidenav();
+  $('#fade-out-target').fadeOut(4000)
 })
